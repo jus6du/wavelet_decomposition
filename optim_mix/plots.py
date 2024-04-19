627f2f8f45b66d8fa522537b2698a48d572bda09
Justine Duval <jus.duval@gmail.com>
2024-04-19 17:27:53 +0200

    optimization notebook

---
+++ optim_mix/plots.py
@@ -0,0 +1,92 @@
+import plotly.graph_objects as go
+import plotly.express as px
+
+from plotly.subplots import make_subplots
+
+colors_dict = {
+    'Wind': 'rgb(255, 128, 128)',        
+    'PV': 'rgb(128, 255, 128)',
+    'Discharge': '#ff7f0e',    
+    'SOC': '#2ca02c',           
+    'Charge': '#9467bd',
+    'Consumption': '#e377c2',          
+    'Dispatchable': 'rgb(128, 128, 255)',       
+    'Curtailment': '#17becf'    
+}
+
+def plot_ts_optim(list_arrays, list_names, country_name, savefig = False):
+    fig = go.Figure()
+    signal_length = len(list_arrays[0])
+    for i in range(len(list_arrays)):
+        fig.add_trace(go.Scatter(x=list(range(signal_length)), y=list_arrays[i], mode='lines', name=list_names[i],marker=dict(color=colors_dict[list_names[i]])))
+
+    fig.update_layout(
+        xaxis_title = 'Time (hour)',
+        yaxis_title='Power (normalized)',
+        title=f'Optimization results {country_name}'
+    )
+    if savefig: 
+        # Save figure
+        fig.write_html(f"figures/{country_name}_optim_full_ts.html")
+
+    # Show figure
+    fig.show()
+    
+    return
+
+def plot_stack_production(PV, wind, dispatch, country_name, colors = colors_dict, savefig=False):
+    # Plot the stacked production
+    fig = go.Figure()
+    signal_length = len(PV)
+    # Add the stacked production trace
+    fig.add_trace(go.Scatter(x=list(range(signal_length)), y=PV, mode='lines', name='PV', stackgroup = 'one',marker=dict(color=colors['PV'])))
+    fig.add_trace(go.Scatter(x=list(range(signal_length)), y=wind, mode='lines', name='Wind', stackgroup = 'one',marker=dict(color=colors['Wind'])))
+    fig.add_trace(go.Scatter(x=list(range(signal_length)), y=dispatch, mode='lines', name='Dispatchable', stackgroup = 'one', marker=dict(color=colors['Dispatchable'])))
+
+    if savefig:
+        fig.write_html(f"figures/{country_name}_optim_stacked_prod_ts.html")
+
+    fig.show()
+    return
+
+def plot_pie_energy(list_energy, country_name, names = ['Wind', 'PV', 'Dispatchable'], colors = ['rgb(255, 128, 128)', 'rgb(128, 255, 128)', 'rgb(128, 128, 255)'], savefig=False):
+    fig = px.pie(names=names,
+                values=list_energy,
+                color_discrete_sequence=colors,  # Utiliser color_discrete_sequence pour définir les couleurs
+                title='Energy Production')
+    fig.update_traces(textposition='inside', textinfo='percent+label')
+    # Write HTML file
+    if savefig:
+        fig.write_html(f"figures/{country_name}_optim_pie_chart.html")
+
+    # Show the plot
+    fig.show()
+    return
+
+def plot_storage(charge, discharge, SOC, country_name, colors = colors_dict, savefig = False):
+    fig = make_subplots(rows=2, cols=1, shared_xaxes=True, subplot_titles=("Charge/Discharge", "State of Charge (SOC)"))
+
+    signal_length = len(charge)
+    fig.add_trace(go.Scatter(x=list(range(signal_length)), y=charge, mode='lines',marker=dict(color=colors_dict['Charge']), name='Charge'),  row=1, col=1)
+    fig.add_trace(go.Scatter(x=list(range(signal_length)), y=discharge, mode='lines',marker=dict(color=colors_dict['Discharge']), name='Disharge'),  row=1, col=1)
+    fig.add_trace(go.Scatter(x=list(range(signal_length)), y=SOC, mode='lines', marker=dict(color=colors_dict['SOC']),name='SOC'), row=2, col=1)
+    
+    fig.update_xaxes(title_text='Time (hour)', row=2, col=1)
+    fig.update_yaxes(title_text='Power (normalized)', row=1, col=1)
+    fig.update_yaxes(title_text='State of Charge', row=2, col=1)
+
+    fig.update_layout(
+        title=f'Charge/Discharge and State of Charge (SOC) for {country_name}',
+        height=600
+    )
+    if savefig: 
+        # Save figure
+        fig.write_html(f"figures/{country_name}_optim_stock_ts.html")
+
+    # Show figure
+    fig.show()
+    return
+
+def get_energy_tot(list_arrays):
+    
+    return
---
+++ optim_mix/utilities.py
@@ -0,0 +1,53 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+'''
+This function :
+    - Interpolate on greater number of points
+    - Normalize each time series for each year
+    - stack all years in a dictionnary : stacked data
+For example  Nyears PV data are accessible in stacked_data[Solaire]
+'''
+def import_excel(path_input_data,input_file, dpd ,ndpd, dpy, interp=True, norm = 'mean'):
+    '''
+    interp : Shall we interpolate drom dpd to ndpd. True or False
+    dpd : data per day
+    ndpd : new data per day
+    Returns a dictionnary with stacked time series over the N years of the excel file. Each year is normalized and interpolated (if true)
+    '''
+    input_data = pd.ExcelFile(path_input_data + input_file)
+
+    df = pd.read_excel(path_input_data + input_file)
+
+    myarray = df.values
+    one_d = myarray.ravel()
+    one_d = one_d.astype(float)
+    mysize = one_d.size
+    assert(mysize % dpd == 0), 'import_excel : Data does not cover an integer number of days'
+
+    # Renormalize to 1MW mean for each full year and discard the rest
+    dataperyear = dpd*dpy
+    nfullyears = int( mysize /(dataperyear) )
+
+    for i in range(nfullyears):
+        sublist = one_d[i * dataperyear: (i + 1) * dataperyear]
+        if norm =='mean':
+            mean = np.mean(sublist)
+        elif norm == 'max':
+            mean = np.max(sublist)
+        one_d[i * dataperyear: (i + 1) * dataperyear] = sublist / mean
+    one_d = one_d[0:nfullyears * dataperyear]
+
+    # Interpolate on the new grid
+
+    if ndpd is not None:
+        ndays = nfullyears*dpy
+        oldx = np.arange(0, ndays, 1./dpd)
+        newx = np.arange(0, ndays, 1./ndpd)
+        newy = np.interp(newx, oldx, one_d)
+    else:
+        newy = one_d
+
+    return newy
