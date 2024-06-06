268e3bf9a9c443f8fe8cd67f59269a028719d0b8
Justine Duval <jus.duval@gmail.com>
2024-06-06 15:52:55 +0200

    fix get coordinates for USA

--- optim_mix/ren_ninja_api.py
+++ optim_mix/ren_ninja_api.py
@@ -4,6 +4,9 @@ import pandas as pd
 import random
 from opencage.geocoder import OpenCageGeocode
 import io
+import geopandas as gpd
+from shapely.geometry import Point
+import numpy as np
 
 # Your Renewable Ninja API token
 api_token = 'e2be88b4f8ae85f401fdf6b205165098e3cf4e37'
@@ -34,6 +37,46 @@ def get_random_coordinates(country, num_locations):
         lat = random.uniform(min_lat, max_lat)
         lon = random.uniform(min_lon, max_lon)
         coordinates.append((lat, lon))
+    return coordinates
+
+def get_regular_coordinates():
+    # Charger les données de la carte du monde
+    world = gpd.read_file(gpd.datasets.get_path('naturalearth_lowres'))
+
+    # Définir les limites de la grille
+    min_lon, min_lat, max_lon, max_lat = -180, -90, 180, 90
+    grid_spacing = 10  # espacement de 10 degrés
+
+    # Générer les points de la grille
+    lons = np.arange(min_lon, max_lon, grid_spacing)
+    lats = np.arange(min_lat, max_lat, grid_spacing)
+    grid_points = [Point(lon, lat) for lon in lons for lat in lats]
+
+    # Créer un GeoDataFrame pour les points de la grille
+    grid_df = gpd.GeoDataFrame(geometry=grid_points)
+
+    # Extraire les centroïdes des pays
+    centroids = world.centroid
+
+    # Créer un GeoDataFrame pour les centroïdes
+    centroids_df = gpd.GeoDataFrame(geometry=centroids)
+
+    # Fusionner les points de la grille et les centroïdes
+    all_points = pd.concat([grid_df, centroids_df], ignore_index=True)
+
+    # Vérifier quels points sont dans les frontières des pays
+    points_in_world = gpd.sjoin(all_points, world, op='within')
+    return points_in_world
+
+def coordinates_in_country(country_name, points_in_world):
+    coordinates = []
+    country_codes = pd.read_csv('../countries_codes_and_coordinates.csv' , sep = ',', index_col = 0)
+    iso_code = country_codes.loc[country_name,'Alpha-3 code' ].split(' ')[1]
+    df_points_in_country = points_in_world[points_in_world['iso_a3']==iso_code ]
+
+    if len(df_points_in_country)!=0:
+        for point in df_points_in_country['geometry']:
+            coordinates.append((point.y, point.x))
     return coordinates
 
 def get_renewable_data(lat, lon, technology, year=2021):
@@ -73,13 +116,19 @@ def get_renewable_data(lat, lon, technology, year=2021):
         raise Exception(f"Error {response.status_code}: {response.text}")
     
 
-def fetch_and_average_data_ren_ninja(country, num_locations, technologies, year=2020, save = False):
-    coordinates = get_random_coordinates(country, num_locations)
+def fetch_and_average_data_ren_ninja(country, num_locations, technologies, points_in_world, year=2020, save = False, coordinates = 'random'):
+    if  coordinates == 'random' :
+        coordinates_list = get_random_coordinates(country, num_locations)
+    elif coordinates =='grid' : 
+        coordinates_list = coordinates_in_country(country, points_in_world)
+        num_locations = len(coordinates_list)
+    else:
+        raise ValueError('Unknown input for coordinates.')
     all_data = {}
 
     for tech in technologies:
         tech_data = []
-        for lat, lon in coordinates:
+        for lat, lon in coordinates_list:
             try:
                 data = get_renewable_data(lat, lon, tech, year)
                 tech_data.append(data)
@@ -91,12 +140,12 @@ def fetch_and_average_data_ren_ninja(country, num_locations, technologies, year=
             all_data[tech] = combined_df
 
         if save : 
-            if os.path.exists(f'../input_time_series/{country}'):
-                pass
-            else :
+            
+            if not os.path.exists(f'../input_time_series/{country}'):
                 os.makedirs(f'../input_time_series/{country}')
             for tech, df in all_data.items():
-                df['electricity'].to_excel(f'../input_time_series/{country}/ren_ninja_{num_locations}_locations_averaged_{tech}_{country}_{year}.xlsx', index=False)
+                
+                df['electricity'].to_excel(f'../input_time_series/{country}/ren_ninja_{num_locations}_{coordinates}_locations_averaged_{tech}_{country}_{year}.xlsx', index=False)
 
     return 
 
